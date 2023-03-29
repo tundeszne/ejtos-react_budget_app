b2057eee9317c4458c6d280f7e2cdc97afa092d7
tundeszne <tundeszilagyine@gmail.com>
2023-03-29 15:04:52 +0200

    Currency dropdown

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,28 @@
 import React, { useContext } from 'react';
 import { AiFillPlusCircle, AiFillMinusCircle, AiFillCloseCircle } from 'react-icons/ai';
-import { FiDelete } from 'react-icons/fi';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
+
+    const handleIncreaseExpense = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'INC_EXPENSE',
+            payload: expense,
+        });
+    };
+
+    const handleDecreaseExpense = () => {
+        dispatch({
+            type: 'DEC_EXPENSE',
+            payload: props.id,
+        });
+    };
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -13,23 +31,11 @@ const ExpenseItem = (props) => {
         });
     };
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
-    };
-
     return (
         <tr>
             <td>{props.name}</td>
             <td>£{props.cost}</td>
-            <td><AiFillPlusCircle color='green' size='2em' onClick={handleDeleteExpense}></AiFillPlusCircle></td>
+            <td><AiFillPlusCircle color='green' size='2em' onClick={handleIncreaseExpense}></AiFillPlusCircle></td>
             <td><AiFillMinusCircle color='red' size='2em' onClick={handleDeleteExpense}></AiFillMinusCircle></td>
             <td><AiFillCloseCircle size='1.5em' onClick={handleDeleteExpense}></AiFillCloseCircle></td>
         </tr>
