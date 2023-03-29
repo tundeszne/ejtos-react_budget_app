a7f19df08f63d434f909d57d40c50ffe6374a3ae
tundeszne <tundeszilagyine@gmail.com>
2023-03-29 10:12:52 +0200

    Add budget component to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Budget from './components/Budget';
 
 import { AppProvider } from './context/AppContext';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                <div className='col-sm'>
+                    <Budget />
+                </div>   
 
                         {/* Add Remaining component here under */}        
 
@@ -21,9 +25,9 @@ const App = () => {
 
                         {/* Add AllocationForm component here under */}        
 
-                </div>
             </div>
         </AppProvider>
     );
 };
+
 export default App;
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,6 +3,7 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    
     return (
         <div className='alert alert-secondary'>
             <span>Budget: £{budget}</span>
