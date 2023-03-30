0aae1d8a7ddd79e4729f7fb4d1d1aac72ecbc0f5
tundeszne <tundeszilagyine@gmail.com>
2023-03-30 19:15:34 +0200

    budget sg

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, currency } = useContext(AppContext);
+    const { budget, currency, dispatch } = useContext(AppContext);
+
+    const initialBudget = budget;
+    const [newBudget, setNewBudget] = useState(initialBudget);
+
+    const submitNewBudget = () => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget,
+        });
+    }
     
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: {currency}{budget}
-            </span>
+            <form onSubmit={submitNewBudget}>
+                Budget: {currency}{budget}
+                <input
+                    type='number'
+                    id='newBudget'
+                    value={newBudget}
+                    style={{ marginLeft: '0rem' , size: 10}}
+                    onChange={(event) => setNewBudget(event.target.value)}
+                    >
+                </input>
+            </form>
         </div>
     );
 };
