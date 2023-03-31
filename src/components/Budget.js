f798fd3fc0acf7ff8026019ea24a7440ef931e9d
tundeszne <tundeszilagyine@gmail.com>
2023-03-31 18:26:12 +0200

    budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,13 +2,22 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, currency, dispatch } = useContext(AppContext);
+    const { budget, currency, dispatch, expenses } = useContext(AppContext);
 
     const initialBudget = budget;
     const [newBudget, setNewBudget] = useState(initialBudget);
 
     const submitNewBudget = (e) => {
         e.preventDefault();
+
+        const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+        
+        if (newBudget < totalExpenses) {
+            alert("You cannot reduce the budget lower than the spending: " + currency + totalExpenses);
+            newBudget=budget;
+            return;
+        }
+
         dispatch({
             type: 'SET_BUDGET',
             payload: newBudget,
@@ -18,12 +27,13 @@ const Budget = () => {
     return (
         <div className='alert alert-secondary'>
             <form onSubmit={submitNewBudget}>
-                Budget: {currency}
+                <label for='newBudget'>Budget: {currency}</label>
                 <input
                     type='number'
                     id='newBudget'
+                    name='newBudget'
                     value={newBudget}
-                    style={{ marginLeft: '0rem' , size: 10}}
+                    // style={{ marginLeft: '0rem' , size: 10}}
                     onChange={(event) => setNewBudget(event.target.value)}
                     >
                 </input>
